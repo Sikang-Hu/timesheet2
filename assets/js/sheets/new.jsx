b6d7240466ab5db293901dc0506f164e46ec9344
sikang hu <sikang_hu@berkeley.edu>
2019-11-09 04:54:44 -0500

    f

--- assets/js/sheets/new.jsx
+++ assets/js/sheets/new.jsx
@@ -153,7 +153,7 @@ class SheetsNew extends React.Component {
 
 function TaskForm(props) {
   let options = _.map(props.jobs, (job) => {return (
-      <option key={job_code} value={job.job_code}>{job.job_code}</option>
+      <option key={job.job_code} value={job.job_code}>{job.job_code}</option>
     );})
 
   return (
