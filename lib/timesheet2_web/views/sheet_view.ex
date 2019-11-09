6eedd4ee5a3f7d46420fe8c38ee604e69cc7200d
sikang hu <sikang_hu@berkeley.edu>
2019-11-09 02:30:10 -0500

    test

--- lib/timesheet2_web/views/sheet_view.ex
+++ lib/timesheet2_web/views/sheet_view.ex
@@ -11,6 +11,7 @@ defmodule Timesheet2Web.SheetView do
   end
 
   def render("sheet.json", %{sheet: sheet}) do
+    IO.inpsect(sheet)
     %{id: sheet.id,
       approve: sheet.approve,
       date: sheet.date}
